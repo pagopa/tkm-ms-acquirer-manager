65a9a1d4c584001291b083593093d44c9c8f8f82
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-06-10 14:03:03 +0200

    pagopa-941 fix blob storage search

--- src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/impl/BinRangeServiceImpl.java
+++ src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/impl/BinRangeServiceImpl.java
@@ -1,21 +1,28 @@
 package it.gov.pagopa.tkm.ms.acquirermanager.service.impl;
 
-import com.azure.storage.blob.*;
-import com.azure.storage.blob.models.*;
-import com.azure.storage.common.sas.*;
-import it.gov.pagopa.tkm.ms.acquirermanager.constant.*;
-import it.gov.pagopa.tkm.ms.acquirermanager.model.response.*;
-import it.gov.pagopa.tkm.ms.acquirermanager.service.*;
-import org.apache.commons.lang3.*;
-import org.apache.commons.lang3.math.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.stereotype.*;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
+import com.azure.storage.common.sas.AccountSasPermission;
+import com.azure.storage.common.sas.AccountSasResourceType;
+import com.azure.storage.common.sas.AccountSasService;
+import com.azure.storage.common.sas.AccountSasSignatureValues;
+import it.gov.pagopa.tkm.ms.acquirermanager.constant.BatchEnum;
+import it.gov.pagopa.tkm.ms.acquirermanager.model.response.LinksResponse;
+import it.gov.pagopa.tkm.ms.acquirermanager.service.BinRangeService;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
-import javax.annotation.*;
-import java.time.*;
-import java.time.format.*;
-import java.time.temporal.*;
-import java.util.*;
+import javax.annotation.PostConstruct;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BinRangeServiceImpl implements BinRangeService {
@@ -61,14 +68,10 @@ public class BinRangeServiceImpl implements BinRangeService {
 
     private List<String> getLinks() {
         List<String> links = new ArrayList<>();
-        String directory = StringUtils.joinWith("_",
-                BatchEnum.BIN_RANGE_GEN,
-                profile,
-                dateFormat.format(Instant.now())
-        );
+        String formatNow = dateFormat.format(Instant.now());
         String completeContainerUrl = client.getBlobContainerUrl();
-        for (BlobItem blobItem : client.listBlobsByHierarchy(directory + "/")) {
-            links.add(completeContainerUrl + "/" + blobItem.getName() + "?" + sas);
+        for (BlobItem blobItem : client.listBlobsByHierarchy(BatchEnum.BIN_RANGE_GEN + "/" + formatNow)) {
+            links.add(String.format("%s/%s?%s", completeContainerUrl, blobItem.getName(), sas));
         }
         return links;
     }
