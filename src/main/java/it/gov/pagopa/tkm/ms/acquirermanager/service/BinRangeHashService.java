4bec22c621be20b55389e7d9fbff712924cd1f55
David Bertoni <David Bertoni>
2021-06-11 15:31:14 +0200

    PagoPA-941 - Tests

--- src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/BinRangeHashService.java
+++ src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/BinRangeHashService.java
@@ -3,7 +3,7 @@ package it.gov.pagopa.tkm.ms.acquirermanager.service;
 import it.gov.pagopa.tkm.ms.acquirermanager.constant.BatchEnum;
 import it.gov.pagopa.tkm.ms.acquirermanager.model.response.LinksResponse;
 
-public interface BinRangeHashingService {
+public interface BinRangeHashService {
 
     LinksResponse getSasLinkResponse(BatchEnum batchEnum);
 
