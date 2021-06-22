dc2556a89fe1057c56a850263586d19aaa6c410e
Giuseppe Ferro <giuseppe.ferro@gft.com>
2021-06-22 17:24:40 +0200

    pagopa-942 fix pgp Utils decrypt

--- src/test/java/it/gov/pagopa/tkm/ms/acquirermanager/util/TestPgpUtils.java
+++ src/test/java/it/gov/pagopa/tkm/ms/acquirermanager/util/TestPgpUtils.java
@@ -19,7 +19,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -64,12 +63,7 @@ class TestPgpUtils {
 
     @Test
     void decrypt_success() throws IOException, PGPException {
-        Path tempFileWithMessagePgp = createTempFileWithMessage();
-        String fileOut = Files.createFile(tempDir.resolve(String.valueOf(random.nextInt()))).toFile().getAbsolutePath();
-        PgpUtils.decrypt(tempFileWithMessagePgp.toFile().getAbsolutePath(), privateKey, PASSPHRASE_CHARS, fileOut);
-        String messageDecrypted = Files.lines(Paths.get(fileOut), StandardCharsets.UTF_8).collect(Collectors.joining(System.lineSeparator()));
-        assertTrue(new File(fileOut).length() > 0);
-        assertEquals(MESSAGE_DECRYPTED_PGP, messageDecrypted);
+        decryptSuccess(MESSAGE_CRYPTED_PGP);
     }
 
     @Test
@@ -80,14 +74,14 @@ class TestPgpUtils {
 
     @Test
     void decrypt_invalidPassphrase() throws IOException {
-        Path tempFileWithMessagePgp = createTempFileWithMessage();
+        Path tempFileWithMessagePgp = createTempFileWithMessage(MESSAGE_CRYPTED_PGP);
         String fileOut = Files.createFile(tempDir.resolve(String.valueOf(random.nextInt()))).toFile().getAbsolutePath();
         assertThrows(PGPException.class, () -> PgpUtils.decrypt(tempFileWithMessagePgp.toFile().getAbsolutePath(), privateKey, "pwd".toCharArray(), fileOut));
     }
 
     @Test
     void decrypt_blankPrivateKey() throws IOException {
-        Path tempFileWithMessagePgp = createTempFileWithMessage();
+        Path tempFileWithMessagePgp = createTempFileWithMessage(MESSAGE_CRYPTED_PGP);
         String fileOut = Files.createFile(tempDir.resolve(String.valueOf(random.nextInt()))).toFile().getAbsolutePath();
         byte[] bytes = "".getBytes();
         String absolutePath = tempFileWithMessagePgp.toFile().getAbsolutePath();
@@ -95,38 +89,19 @@ class TestPgpUtils {
     }
 
     @Test
-    void encrypt_success() throws PGPException {
-        String l="-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
-                "\n" +
-                "mQINBGCeiV4BEACvYSZO1XG4UbS1SDUDs1vcX2/iJBBwcVpllSFeeHO0npqDqpN7\n" +
-                "bBX+ns7kn6AuyFSo0AT1u0s2Av/Qv8z7piJbRnfRc7MwMpMp7PvCVymDhyUeSWkq\n" +
-                "DlbcqgzPJwlkYvVuWOmADJoJIqnXxCPgSSPe+RbMwuhWTlg5Ss1gpBj+8S/dJcjR\n" +
-                "+xEAai8HoqrJplQ3ctsSBo3SnDon0q17JMFOtrf1K8tqOoa+R4DwEByaUWYKP8Xf\n" +
-                "EPo1WEonqelsipzkCWq9U0CKtJnavjy5enHr/2+/Dw2EkTZCQAJAg5P25suUtcog\n" +
-                "LPfLi1b8ywfD6aLXcIfKGZbjU1MnsM92KZ0Zx/gRKTBGpobf06t7O2OKygaJsZZj\n" +
-                "t6qbTKTq8tnOixABlW6PJDV4UvuuZhL1EXDvooRJ3PtL9Bq9zm6OW5rgcQT1kV8D\n" +
-                "D/fZ/eTAXZZbNfdyLPq0CSyBS7w3besipjf16Gf3kBJSsBN5USU2URMddl5RSRVx\n" +
-                "1SZknWSmtFR4L6Qb5hRXX1d+Za1vdOuq4vUkRUPbDJ/5QLbVgnDhyLdKsIrj6lbg\n" +
-                "iQtMrgDho7hSAnNh+AccwbuCjS7PTen848N3Gi6R+5sHIIA+AXy1It2srfyTITQ2\n" +
-                "e+OQ6BjezJezWBPcLHLpPST9ym2omktHxgnwACYx8/UxFzjDdQewTZ3lLQARAQAB\n" +
-                "tBF0a21SZWFkUXVldWVMb2NhbIkCVAQTAQgAPhYhBAjB5dPFfwRPGYAI/Pfg+hqa\n" +
-                "4+zzBQJgnoleAhsPBQkSHcvSBQsJCAcCBhUKCQgLAgQWAgMBAh4BAheAAAoJEPfg\n" +
-                "+hqa4+zzQM4QAKsOM+su0YSubQ6K2GTlh8JCjjzuZpWPuVvxBOYI9yjc7kspQ5Mx\n" +
-                "u5Rt1PhcRqWiViLstfQ0Xb6dJ5/Z465+2cB+ob1RFf7HqmjSTji+CrhOsaO6rWFL\n" +
-                "qxWeWX71GjzFQrQGw4AM1O16l2ARG8gmkN1xdjqgg0X0fKR5JbMF0vc2TNqB/pjE\n" +
-                "Gi0LoBvqD4fmsICaBi4ZO2l4A7Dl1YuyPjdN6BImjTSNjAbdPfBT96Bdlg3KLkcw\n" +
-                "xXUwiBNLVr6s9AknMt6SlHING9EjyT+2yXiVjrgX+ZF72NsCU+H2XKHGeEg8h8AU\n" +
-                "s5ZsWcUoXMoiMQcStPRWZLwqpg7ormxusnGGyGnUGjvZOQqs5MSlK8+49jMO5QrG\n" +
-                "Nd3ixoTxmj8NbMbGUlTZIgnlx+f5jjcPTGS+4BXU7+7fbMoMc4yOMT/obiJXPaZ8\n" +
-                "EHjzdLnp47oyCwxtfpmQDJ/lIU5kw0riHM9tAHNrAfp5A9+qG4R2MzdNJnAYKWPY\n" +
-                "IQSI6k+H9XLnu91tiqjFej21srQLJP5x5ujit4xDTMtINc0sBvq0sMZYJON95GoV\n" +
-                "E/Dg+deFSCtgHg7FyN3oZtwT2jpKc04L/4BLSe7krqUMmk6JQHPFuEoFmHiNIEyb\n" +
-                "VeKlHbrqybEAxPCkS0JnV6IfTH7ukpNxRSkuBVsNvpCC7zA2kwrUzfO7\n" +
-                "=dRS1\n" +
-                "-----END PGP PUBLIC KEY BLOCK-----\n";
-        byte[] encrypt = PgpUtils.encrypt(MESSAGE_DECRYPTED_PGP.getBytes(), l.getBytes(), true);
-        System.out.println(new String(encrypt));
+    void encrypt_success() throws PGPException, IOException {
+        byte[] encrypt = PgpUtils.encrypt(MESSAGE_DECRYPTED_PGP.getBytes(), publicKey, true);
         assertTrue(encrypt.length > 0);
+        decryptSuccess(new String(encrypt));
+    }
+
+    private void decryptSuccess(String encrypt) throws IOException, PGPException {
+        Path tempFileWithMessagePgp = createTempFileWithMessage(encrypt);
+        String fileOut = Files.createFile(tempDir.resolve(String.valueOf(random.nextInt()))).toFile().getAbsolutePath();
+        PgpUtils.decrypt(tempFileWithMessagePgp.toFile().getAbsolutePath(), privateKey, PASSPHRASE_CHARS, fileOut);
+        String messageDecrypted = Files.lines(Paths.get(fileOut), StandardCharsets.UTF_8).collect(Collectors.joining(System.lineSeparator()));
+        assertTrue(new File(fileOut).length() > 0);
+        assertEquals(MESSAGE_DECRYPTED_PGP, messageDecrypted);
     }
 
     @Test
@@ -134,10 +109,10 @@ class TestPgpUtils {
         assertThrows(PGPException.class, () -> PgpUtils.encrypt(MESSAGE_DECRYPTED_PGP.getBytes(), "".getBytes(), true));
     }
 
-    private Path createTempFileWithMessage() throws IOException {
+    private Path createTempFileWithMessage(String message) throws IOException {
         Path tempFile = Files.createFile(tempDir.resolve(String.valueOf(random.nextInt())));
         try (FileWriter myWriter = new FileWriter(tempFile.toFile())) {
-            myWriter.write(MESSAGE_CRYPTED_PGP);
+            myWriter.write(message);
         }
         return tempFile;
     }
