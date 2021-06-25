b6986fcaa5065ec2905cfabad0e572590718b9cd
David Bertoni <David Bertoni>
2021-06-25 12:54:05 +0200

    PagoPa-942 - Moving PgpUtils

--- src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/impl/ProducerServiceImpl.java
+++ src/main/java/it/gov/pagopa/tkm/ms/acquirermanager/service/impl/ProducerServiceImpl.java
@@ -2,18 +2,15 @@ package it.gov.pagopa.tkm.ms.acquirermanager.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import it.gov.pagopa.tkm.ms.acquirermanager.model.dto.ReadQueue;
 import it.gov.pagopa.tkm.ms.acquirermanager.service.ProducerService;
-import it.gov.pagopa.tkm.ms.acquirermanager.util.PgpUtils;
+import it.gov.pagopa.tkm.service.*;
 import lombok.extern.log4j.Log4j2;
 import org.bouncycastle.openpgp.PGPException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
 
 @Service
 @Log4j2
@@ -34,7 +31,7 @@ public class ProducerServiceImpl implements ProducerService {
     public void sendMessage(ReadQueue readQueue) throws JsonProcessingException, PGPException {
         String message = mapper.writeValueAsString(readQueue);
         log.trace("Forwarding message to queue: " + message);
-        byte[] encryptedMessage = PgpUtils.encrypt(message.getBytes(), readQueuePubPgpKey, true);
+        byte[] encryptedMessage = PgpStaticUtils.encrypt(message.getBytes(), readQueuePubPgpKey, true);
 
         kafkaTemplate.send(readQueueTopic, new String(encryptedMessage));
     }
